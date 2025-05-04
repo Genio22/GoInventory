4ac9876465e9ec1cc0ec78e410ad372adaecb2c9
Genio22 <relaquantisten@gmail.com>
2025-05-04 20:58:21 +0600

    first commit

---
+++ src/main/java/Dashboard/Dashboard_app.java
@@ -0,0 +1,16 @@
+package Dashboard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Dashboard_app extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
