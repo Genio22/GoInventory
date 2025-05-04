d609ce95fd7ad06a494bb50c24e6b66853bdcd25
Genio22 <relaquantisten@gmail.com>
2025-05-04 20:54:26 +0600

    first commit

---
+++ src/main/java/login/LoginController.java
@@ -0,0 +1,49 @@
+package login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+        private String username, password;
+        @FXML
+        private PasswordField pass_text;
+
+        @FXML
+        private AnchorPane sidePanel;
+
+        @FXML
+        private TextField user_text;
+
+    @FXML
+    void signin_click(MouseEvent event) {
+        this.username = user_text.getText();
+        this.password = pass_text.getText();
+
+        if ("Admin".equals(username) && "admin".equals(password)) {
+            System.out.println("Login Successful");
+            System.out.println(
+                    "Username: " + username + " Password: " + password
+            );
+            System.out.println("Welcome Admin");
+        } else {
+            System.out.println("Invalid credentials. Access denied.");
+            System.out.println(
+                    "Username: " + username + " Password: " + password
+            );
+        }
+    }
+
+    @FXML
+    void username(ActionEvent event) {
+        this.username = user_text.getText();
+    }
+
+    @FXML
+    void password(ActionEvent event) {
+        this.password = pass_text.getText();
+    }
+}
