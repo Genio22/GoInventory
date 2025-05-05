80beb606f7f6be36b58d201e92e032a9716671ae
Genio22 <relaquantisten@gmail.com>
2025-05-05 15:28:12 +0600

    first commit

--- src/main/java/Dashboard/Dashboard_controller.java
+++ src/main/java/Dashboard/Dashboard_controller.java
@@ -1,5 +1,24 @@
 package Dashboard;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
 public class Dashboard_controller {
+
+    @FXML
+    private Button Dashboard;
+
+    @FXML
+    private Button Invoices;
+
+    @FXML
+    private Button Manage_Users_Role;
+
+    @FXML
+    private MenuButton Order_Management;
+
+    @FXML
+    private MenuButton Product_Inventory;
 
 }
--- src/main/java/Luanch/GoInventory.java
+++ src/main/java/Luanch/GoInventory.java
@@ -1,4 +1,9 @@
 package Luanch;
 
+import Dashboard.Dashboard_app;
+
 public class GoInventory {
+    public static void main(String[] args) {
+        Dashboard_app.main(args);
+    }
 }
